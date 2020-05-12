7335bf14346ed55df957c8235dba489f8792efa7
Ruonan Y <joyceyang0730@gmail.com>
2020-05-11 19:48:38 -0700

    run npm build in client

---
+++ client/build/precache-manifest.d403a43dc24bfcba2f50ad1e7fcbd1a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3efaaf5e7b4844cc19fc",
+    "url": "/static/css/main.bcd9fe3d.chunk.css"
+  },
+  {
+    "revision": "3efaaf5e7b4844cc19fc",
+    "url": "/static/js/main.e9f383ed.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3b7efb486d339c2f93b2",
+    "url": "/static/js/2.b953f459.chunk.js"
+  },
+  {
+    "revision": "7cb75b25fd68a43844c9c23d6a5e2862",
+    "url": "/index.html"
+  }
+];
